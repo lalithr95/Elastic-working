19e83ac3ad2faf0b278219b617fb7844614370b7
Lalith <lalithr95@gmail.com>
2016-01-13 21:37:23 +0530

    initial rails code

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destory
+	end
+end
