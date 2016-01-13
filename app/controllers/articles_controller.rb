6ff5ea80ef302df9d93b18480f49230d18890d95
Lalith <lalithr95@gmail.com>
2016-01-13 21:40:49 +0530

    article model and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,20 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
+		@article = Article.find(params[:id])
 	end
 
 	def create
+		@article = Article.new article_params
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
 	end
 
-	def update
-	end
-
-	def destory
+	private
+	def article_params
+		params.require(:article).permit(:text, :title)
 	end
 end
